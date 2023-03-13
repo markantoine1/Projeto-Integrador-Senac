6226730f61ef3cf3d218e8bfbe85052fdc6b759b
Mark Antoine <98465925+markantoine1@users.noreply.github.com>
2023-03-13 08:30:25 -0300

    First commit

---
+++ src/test/java/com/example/senac/alocacao/AlocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senac.alocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
